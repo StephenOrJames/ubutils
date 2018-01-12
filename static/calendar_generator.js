89e634ed0d784b285cf745b6d7acef9db5c0c546
Stephen James <stephenorjames@gmail.com>
2018-01-11 23:29:09 -0500

    Support loading semester and course from URL parameters

--- static/calendar_generator.js
+++ static/calendar_generator.js
@@ -1,36 +1,42 @@
 var app = new Vue({
-  el: "main",
+  el: 'main',
 
   data: {
     isLoading: false, // indicates that we are waiting for an external response
     search: {
-      query: "",
-      results: "",
+      query: '',
+      results: '',
       cache: {},
     },
     courses: [],
-    semester: "spring",
+    semesters: {
+      all: {
+        'fall': 'Fall',
+        'winter': 'Winter',
+        'spring': 'Spring',
+        'summer': 'Summer',
+      },
+      selected: 'spring',
+    },
   },
 
   methods: {
     displayResults(abbr, num) {
-      this.search.results = this.search.cache[this.semester][abbr][num];
+      this.search.results = this.search.cache[this.semesters.selected][abbr][num];
     },
 
     generateSummary(course) {
       return course.catalog.description +
-          " (" + course.catalog.abbr + " " + course.catalog.num + "): " +
+          ` (${course.catalog.abbr} ${course.catalog.num}): ` +
           this.generateSummaryWhen(course.when) +
-          " with " + course.instructor +
-          " in " + course.room + ".";
+          ` with ${course.instructor} in ${course.room}.`;
     },
 
     generateSummaryWhen(when) {
       let whens = [];
       for (let i = 0; i < when.length; ++i) {
         whens.push(
-            when[i].pattern +
-            " (" + when[i].dates.start + " to " + when[i].dates.end + ")"
+            `${when[i].pattern} (${when[i].dates.start} to ${when[i].dates.end})`
         );
       }
       return whens.join(" and ");
@@ -40,28 +46,29 @@ var app = new Vue({
       // Parse for departmental abbreviation and course number
       const query = this.search.query.toUpperCase().match(/([A-Z]+)\s*(\d+)/);
       if (query === null) {
-        console.log("Invalid search query");
+        console.log('Invalid search query');
         return;
       }
       const abbr = query[1];
       const num = query[2];
+      const semester = this.semesters.selected;
 
       // Check the cache first
-      if (this.semester in this.search.cache
-          && abbr in this.search.cache[this.semester]
-          && num in this.search.cache[this.semester][abbr]) {
+      if (semester in this.search.cache
+          && abbr in this.search.cache[semester]
+          && num in this.search.cache[semester][abbr]) {
         this.displayResults(abbr, num);
         return;
       }
 
       // Fetch if not in cache
       this.isLoading = true;
-      fetch("https://prv-web.sens.buffalo.edu/apis/schedule2/schedule2/courses" +
-            `?semester=${this.semester}&abbr=${abbr}&num=${num}`)
+      fetch('https://prv-web.sens.buffalo.edu/apis/schedule2/schedule2/courses' +
+            `?semester=${semester}&abbr=${abbr}&num=${num}`)
           .then((response) => response.json())
           .then((response) => {
             let results;
-            if ("courses" in response) {
+            if ('courses' in response) {
               results = response.courses;
               for (let i = 0; i < results.length; ++i) {
                 results[i].summary = this.generateSummary(results[i]);
@@ -72,20 +79,20 @@ var app = new Vue({
             }
 
             // Update cache
-            if (!(this.semester in this.search.cache)) {
-              this.search.cache[this.semester] = {};
+            if (!(semester in this.search.cache)) {
+              this.search.cache[semester] = {};
             }
-            if (!(abbr in this.search.cache[this.semester])) {
-              this.search.cache[this.semester][abbr] = {};
+            if (!(abbr in this.search.cache[semester])) {
+              this.search.cache[semester][abbr] = {};
             }
-            this.search.cache[this.semester][abbr][num] = results;
+            this.search.cache[semester][abbr][num] = results;
 
             this.displayResults(abbr, num);
           })
           .then(() => { this.isLoading = false; })
           .catch((error) => {
             this.search.results = [];
-            console.log("Error: " + error);
+            console.log('Error: ' + error);
           });
     },
 
@@ -96,20 +103,43 @@ var app = new Vue({
 
     unselect(course) {
       course.isSelected = false;
-      let index = this.courses.indexOf(course);
+      const index = this.courses.indexOf(course);
       this.courses.splice(index, 1);
     },
 
     generateCalendar() {
-      let input = document.createElement("input");
-      input.name = "courses";
+      const input = document.createElement('input');
+      input.name = 'courses';
       input.value = JSON.stringify(this.courses);
-      let form = document.createElement("form");
-      form.method = "POST";
-      form.style.display = "none";
+      const form = document.createElement('form');
+      form.method = 'POST';
+      form.style.display = 'none';
       form.appendChild(input);
       document.body.appendChild(form);
       form.submit();
     },
-  }
+  },
+
+  mounted() {
+    if (window.URL === undefined) return;
+
+    const url = new URL(document.location.href);
+    const params = url.searchParams;
+    const course = params.get('course');
+    let semester = params.get('semester');
+
+    if (semester !== null) {
+      semester = semester.toLowerCase();
+      if (semester in this.semesters.all) {
+        this.semesters.selected = semester;
+      } else {
+        console.log('Invalid semester in URL');
+      }
+    }
+
+    if (!!course) {
+      this.search.query = course;
+      this.doSearch();
+    }
+  },
 });
